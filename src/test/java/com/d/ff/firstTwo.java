4540c017504e24b13e61b17d40de2d4d5727066a
a198401drr <a198401drr@aliyun.com>
2018-07-13 14:59:05 +0800

    web test

---
+++ src/test/java/com/d/ff/firstTwo.java
@@ -0,0 +1,29 @@
+package com.d.ff;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class firstTwo {
+	
+  @Test
+  public void test1() {
+	  System.out.println("first test");
+  }
+  
+  @Test
+  public void test2() {
+	  System.out.println("second test");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("before class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("after class");
+  }
+
+}
